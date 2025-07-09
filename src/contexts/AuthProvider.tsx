89e489aef75c07a9502a2122528e590ef8b2957a
Larry Ling <larryling@Larrys-MacBook-Pro.local>
2025-07-09 15:33:25 +0800

    implemented AuthProvider functions

---
+++ src/contexts/AuthProvider.tsx
@@ -0,0 +1,151 @@
+import {
+  type PropsWithChildren,
+  useCallback,
+  useEffect,
+  useMemo,
+  useState,
+} from "react";
+import type { UserData } from "../lib/types.ts";
+import { AuthContext } from "./AuthContext.tsx";
+import supabase from "@/services/supabase.ts";
+
+export function AuthProvider({ children }: PropsWithChildren) {
+  const [user, setUser] = useState<UserData | null>(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const signup = useCallback(async (email: string, password: string) => {
+    try {
+      const {
+        data: { user },
+        error,
+      } = await supabase.auth.signUp({
+        email,
+        password,
+      });
+
+      if (error || !user) {
+        throw error;
+      }
+
+      setUser({
+        id: user.id,
+        email: user.email ?? "",
+      });
+    } catch (error) {
+      console.error("Login error:", error);
+      setUser(null);
+    } finally {
+      setIsLoading(false);
+    }
+  }, []);
+
+  const login = useCallback(async (email: string, password: string) => {
+    try {
+      const {
+        data: { user },
+        error,
+      } = await supabase.auth.signInWithPassword({
+        email,
+        password,
+      });
+
+      if (error || !user) {
+        throw error;
+      }
+
+      setUser({
+        id: user.id,
+        email: user.email ?? "",
+      });
+    } catch {
+      setUser(null);
+    } finally {
+      setIsLoading(false);
+    }
+  }, []);
+
+  const logout = useCallback(async () => {
+    await supabase.auth.signOut();
+    setUser(null);
+  }, []);
+
+  const requestPasswordReset = useCallback(async (email: string) => {
+    try {
+      const { error } = await supabase.auth.resetPasswordForEmail(email, {
+        redirectTo: `${import.meta.env.VITE_APP_URL}/reset-password`,
+      });
+
+      if (error) {
+        throw error;
+      }
+    } catch (error) {
+      console.error("Error requesting password reset:", error);
+    }
+  }, []);
+
+  const updatePassword = useCallback(async (password: string) => {
+    try {
+      const { error } = await supabase.auth.updateUser({ password });
+
+      if (error) {
+        throw error;
+      }
+    } catch (error) {
+      console.error("Error updating password:", error);
+    }
+  }, []);
+
+  useEffect(() => {
+    const loadUser = async () => {
+      try {
+        const {
+          data: { user },
+          error,
+        } = await supabase.auth.getUser();
+
+        if (error || !user) {
+          throw error;
+        }
+
+        setUser({
+          id: user.id,
+          email: user.email ?? "",
+        });
+      } catch (error) {
+        console.error("Error requesting password reset:", error);
+        setUser(null);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    loadUser();
+  }, []);
+
+  const value = useMemo(
+    () => ({
+      user,
+      isLoading,
+      signup,
+      login,
+      logout,
+      requestPasswordReset,
+      updatePassword,
+    }),
+    [
+      user,
+      isLoading,
+      signup,
+      login,
+      logout,
+      requestPasswordReset,
+      updatePassword,
+    ],
+  );
+
+  return (
+    <AuthContext.Provider value={value}>
+      {isLoading && children}
+    </AuthContext.Provider>
+  );
+}
---
+++ src/lib/types.ts
@@ -0,0 +1,30 @@
+import type { Database } from "../../database.types";
+
+export type UserData = {
+  id: string;
+  email: string;
+};
+
+export type Player = Database["public"]["Tables"]["players"]["Row"];
+
+export type TrainingBlock =
+  Database["public"]["Tables"]["training_blocks"]["Row"];
+
+export type Availability = Pick<
+  TrainingBlock,
+  "day" | "start" | "start_int" | "end" | "end_int"
+>;
+
+export type Days = Database["public"]["Enums"]["days"];
+
+export type Positions = Database["public"]["Enums"]["positions"];
+
+export type AuthContextType = {
+  user: UserData | null;
+  isLoading: boolean;
+  signup: (email: string, password: string) => Promise<void>;
+  login: (email: string, password: string) => Promise<void>;
+  logout: () => Promise<void>;
+  requestPasswordReset: (email: string) => Promise<void>;
+  updatePassword: (password: string) => Promise<void>;
+};
