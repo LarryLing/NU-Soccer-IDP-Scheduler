4feb36bb78c52abe2ba89e0ba117240be155da61
Larry Ling <larryling.main@gmail.com>
2025-09-04 17:11:51 -0500

    minor changes

--- vite.config.ts
+++ vite.config.ts
@@ -23,12 +23,4 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  define: {
-    "process.env.VITE_SUPABASE_URL": JSON.stringify(
-      process.env.VITE_SUPABASE_URL,
-    ),
-    "process.env.VITE_SUPABASE_ANON_KEY": JSON.stringify(
-      process.env.VITE_SUPABASE_ANON_KEY,
-    ),
-  },
 })
